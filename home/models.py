3d7209490f279d139630c4fc05d45423dec6ee23
Roy Magnussen <roysgamesandmore@gmail.com>
2021-04-07 16:33:41 +0100

    Create Gallery Image model
    Created the Gallery Image model which allows the owner/admin to upload images to the database so that they can be used in the gallery page.
    
    When the image is uploaded, it is resized to a more manageable size (300 x 500) or as close to that as possible. This is to reduce the size of the images which also means the loading times are faster.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
+from PIL import Image
 from django.db import models
 
 # Create your models here.
+
+
+class GalleryImage(models.Model):
+    name = models.CharField(
+        verbose_name='Name', max_length=255, help_text='255 characters or less.')
+    image = models.ImageField(verbose_name='Image', upload_to='gallery_images')
+
+    class Meta:
+        verbose_name = 'Gallery Image'
+        verbose_name_plural = 'Gallery Images'
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __init__(self, *args, **kwargs):
+        super(GalleryImage, self).__init__(*args, **kwargs)
+
+        img = Image.open(self.image)
+        img.thumbnail((300, 500))
+        img.save(self.image)
