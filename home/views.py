ad4f9032efe72cfffc5b9ca2fbc91bd7cebeb99a
Roy Magnussen <roysgamesandmore@gmail.com>
2021-04-12 22:05:24 +0100

    Add gallery images to home view

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import GalleryImage
 
 # Create your views here.
 
 
 def home(request):
+    gallery_images = GalleryImage.objects.all().order_by('-id')
     context = {
         'title': 'Home',
+        'gallery_images': gallery_images[:6],
     }
 
     return render(request, 'home/index.html', context)
