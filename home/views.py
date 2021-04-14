cd970c38f320dd84d4651652e5808de7f148c741
Roy Magnussen <roysgamesandmore@gmail.com>
2021-04-14 22:00:05 +0100

    Add reviews to context dictionary

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import GalleryImage
+from reviews.models import Review
 
 # Create your views here.
 
 
 def home(request):
     gallery_images = GalleryImage.objects.all().order_by('-id')
+    reviews = Review.objects.all().order_by('-id')
     context = {
         'title': 'Home',
         'gallery_images': gallery_images[:6],
+        'reviews': reviews[:6],
     }
 
     return render(request, 'home/index.html', context)
